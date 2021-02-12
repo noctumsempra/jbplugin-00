ac9000d33941e78c0468004c1ae046e747fc8a55
GitHub Action <action@github.com>
2021-02-12 16:43:47 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "jbplugin-00"
--- src/main/kotlin/com/github/noctumsempra/jbplugin00/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/noctumsempra/jbplugin00/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.noctumsempra.jbplugin00.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.noctumsempra.jbplugin00.MyBundle
 
 class MyApplicationService {
 
